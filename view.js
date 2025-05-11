535df6a09705b03bf71384c9d8dd25bb52952613
Carlos-RT <carlos.ramirez02@unicatolica.edu.co>
2025-05-11 04:36:41 -0500

    nueva vista

--- view.js
+++ view.js
@@ -1,7 +1,5 @@
-// view.js
 const container = document.getElementById("quote-container");
-const button = document.getElementById("get-quote");
-const input = document.getElementById("birthdate");
+const button = document.getElementById("consultar");
 
 export function disableButton() {
   button.disabled = true;
@@ -16,33 +14,17 @@ export function clearView() {
   container.classList.remove("visible", "fade-out");
 }
 
-export function renderHoroscope(message) {
-  container.textContent = message;
+export function renderHoroscope({ horoscope, emoji }) {
+  container.innerHTML = `<p>${emoji} ${horoscope}</p>`;
   container.classList.remove("fade-out");
   container.classList.add("visible");
 }
 
-export function renderError(msg = "Error al obtener el horóscopo.") {
-  container.textContent = msg;
+export function renderError(message) {
+  container.textContent = message;
   container.classList.add("visible");
 }
 
 export function fadeOut() {
   container.classList.add("fade-out");
-}
-
-export function isValidDateFormat(dateStr) {
-  return /^\d{2}-\d{2}-\d{4}$/.test(dateStr);
-}
-
-export function onDateInput(callback) {
-  input.addEventListener("input", callback);
-}
-
-export function onConsultClick(callback) {
-  button.addEventListener("click", callback);
-}
-
-export function getInputDate() {
-  return input.value;
 }
